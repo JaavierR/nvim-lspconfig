848bd2fb27b457a05ca0e471a5289ef612c9a47d
Javier Reyes <javier.reys@gmail.com>
2023-06-05 00:13:22 -0400

    fix(astro): match astro config with volar

--- lua/lspconfig/server_configurations/astro.lua
+++ lua/lspconfig/server_configurations/astro.lua
@@ -5,16 +5,23 @@ local function get_typescript_server_path(root_dir)
   return project_root and (util.path.join(project_root, 'node_modules', 'typescript', 'lib')) or ''
 end
 
+local astro_init_options = {
+  typescript = {
+    tsdk = ''
+  }
+}
+
 return {
   default_config = {
     cmd = { 'astro-ls', '--stdio' },
     filetypes = { 'astro' },
     root_dir = util.root_pattern('package.json', 'tsconfig.json', 'jsconfig.json', '.git'),
-    init_options = {
-      typescript = {},
-    },
+    init_options = astro_init_options,
     on_new_config = function(new_config, new_root_dir)
-      if vim.tbl_get(new_config.init_options, 'typescript') and not new_config.init_options.typescript.tsdk then
+      if new_config.init_options
+          and new_config.init_options.typescript
+          and new_config.init_options.typescript.tsdk == ''
+      then
         new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
       end
     end,
@@ -28,8 +35,5 @@ https://github.com/withastro/language-tools/tree/main/packages/language-server
 npm install -g @astrojs/language-server
 ```
 ]],
-    default_config = {
-      root_dir = [[root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git")]],
-    },
   },
 }
